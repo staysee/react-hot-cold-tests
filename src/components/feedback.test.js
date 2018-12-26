1fce9ed851cde72b3c14c94567a0ea5564526589
Stacey Sugiono <sugiono.stacey@gmail.com>
2018-12-26 13:13:04 -0800

    shallow rendering the components

--- src/components/feedback.test.js
+++ src/components/feedback.test.js
@@ -1,4 +1,11 @@
 import Enzyme from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
+import { isMainThread } from 'worker_threads';
 
 Enzyme.configure({adapter: new Adapter()});
+
+describe('Feedback', () => {
+    it ('Renders without crashing', () => {
+        shallow(<Feedback />);
+    });
+})
--- src/components/guess-section.test.js
+++ src/components/guess-section.test.js
@@ -2,3 +2,9 @@ import Enzyme from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
 Enzyme.configure({adapter: new Adapter()});
+
+describe('<GuessSection />', () => {
+    it ('Render without crashing', () => {
+        shallow(<GuessSection />);
+    })''
+})
