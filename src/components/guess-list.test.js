597ccdfdcb2a87ccd188aef49880685590d44103
Stacey Sugiono <sugiono.stacey@gmail.com>
2018-12-28 18:26:17 -0800

    added guess list test

--- src/components/guess-list.test.js
+++ src/components/guess-list.test.js
@@ -7,4 +7,15 @@ describe('GuessList', () => {
     it ('Renders without crashing', () => {
         shallow(<GuessList guesses={[]}/>);
     })
+
+    it('Renders a list of guesses', () => {
+        const numbers = [4, 90, 25];
+        const wrapper = shallow(<GuessList guesses={numbers} />);
+        const guesses = wrapper.find('li');
+
+        expect(guesses.length).toEqual(numbers.length);
+        numbers.forEach((number, index) => {
+            expect(guesses.at(index).text()).toEqual(number.toString());
+        })
+    })
 })
