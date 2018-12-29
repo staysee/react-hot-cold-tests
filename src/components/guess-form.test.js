3535a633823d066588af85991388c766c944f135
Stacey Sugiono <sugiono.stacey@gmail.com>
2018-12-28 18:16:20 -0800

    added guess-form test

--- src/components/guess-form.test.js
+++ src/components/guess-form.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {shallow} from 'enzyme';
+import {shallow, mount} from 'enzyme';
 
 import GuessForm from './guess-form';
 
@@ -7,4 +7,24 @@ describe('<GuessForm />', () => {
     it ('Renders without crashing', () => {
         shallow(<GuessForm />);
     })
+
+    it('Should trigger the onMakeGuess callback when the form is submitted', () => {
+        const callback = jest.fn();
+        const wrapper = mount(<GuessForm onMakeGuess={callback} />);
+        const guess = 5;
+
+        wrapper.find('input[type="number"]').instance().value = guess;
+        wrapper.simulate('submit');
+        expect(callback).toHaveBeenCalledWith(guess.toString());
+    })
+
+    it('Should clear the input when the form is submitted', () => {
+        const wrapper = mount(<GuessForm />)
+        const input = wrapper.find('input[type="number"]');
+        
+        input.instance().value = 5;
+        wrapper.simulate('submit');
+        expect(input.instance().value).toEqual('');
+    })
+
 })
